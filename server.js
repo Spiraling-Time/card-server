40f2d27924b0a72b8a7cd98bf23cdadb8337cac3
Spiraling-Time <yairmeirnoam.peck@gmail.com>
2026-05-25 20:33:52 -0400

    Bind server to all network interfaces

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const http = require("http");
 const WebSocket = require("ws");
 
 const server = http.createServer();
+
 const wss = new WebSocket.Server({ server });
 
 const PORT = process.env.PORT || 8080;
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
     console.log("Server running on port", PORT);
 });
 
